0806a49a6b175e4dee9c04fc5ad8b05f523b9683
Khaled Sarraj <khaledinat@gmail.com>
2021-02-22 17:29:56 +0100

    JaxWsConsumerJSE/ServiceJaxWsJSE

---
+++ PremierServiceJaxWsJSE/src/main/java/fr/eql/ai108/webservice/Service.java
@@ -0,0 +1,27 @@
+package fr.eql.ai108.webservice;
+
+import javax.jws.WebService;
+
+import fr.eql.ai108.entity.Personne;
+
+@WebService(targetNamespace = "http://ai108.microsoft.com", endpointInterface = "fr.eql.ai108.webservice.IService", serviceName = "Ai108Service", portName = "Ai108Port")
+public class Service implements IService {
+
+	@Override
+	public int add(int i, int j) {
+		return i + j;
+	}
+
+	@Override
+	public double pow(double a, double b) {
+		// TODO Auto-generated method stub
+		return Math.pow(a, b);
+	}
+
+	@Override
+	public Personne testSerialisation(Personne personne) {
+		personne.setName(personne.getName().toUpperCase());
+		return personne;
+	}
+
+}
